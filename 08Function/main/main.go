5ef82cb617e30388840f02c41a2ad9e5a55f88ce
马靖玮 <majingwei@majingweideMBP.lan>
2020-11-03 00:03:26 +0800

    func part2

--- 08Function/main/main.go
+++ 08Function/main/main.go
@@ -6,6 +6,13 @@ import (
 	"../utils"
 )
 
+var (
+	//使用方式3:全局匿名函数，将匿名函数赋值给一个全局变量
+	Func1 = func(n1, n2 int) int {
+		return n1 * n2
+	}
+)
+
 //1.包的使用
 func packageExample() {
 	/*utils包的使用*/
@@ -28,7 +35,85 @@ func returnExample() {
 	res1, res2 := multiReturn(1, 2)
 	fmt.Printf("res1=%d,res2=%d\n", res1, res2)
 }
+
+//3.递归
+
+//4.init函数
+/*
+每一个源文件都可以包含一个init函数，该函数在main函数执行前被Go运行框架调用；
+init函数细节：
+1.一个源文件内的执行顺序：全局变量>init()>main();
+2.init函数作用，就是完成一些初始化工作；
+3.该源文件import的文件的变量和init函数先于本文件的变量和init函数执行；
+*/
+func init() {
+	fmt.Println("Call init func before main func.")
+}
+
+//5.匿名函数
+/*
+Go支持匿名函数，就是没有名字的函数；如果希望某个函数只调用一次，则考虑使用匿名函数；当然也可以多次调用；
+*/
+func anonymousFunc() {
+	//使用方式1:定义即调用，只能调用一次；
+	res := func(n1, n2 int) int {
+		return n1 + n2
+	}(10, 20)
+	fmt.Println(res)
+	//使用方式2:将匿名函数赋值给一个变量，实现多次调用；
+	a := func(n1, n2 int) int {
+		return n1 + n2
+	}
+	res = a(10, 20)
+	fmt.Println(res)
+	res = a(90, 30)
+	fmt.Println(res)
+	//使用方式3:全局匿名函数，将匿名函数赋值给一个全局变量
+	res = Func1(5, 5)
+	fmt.Println(res)
+}
+
+//6.闭包
+/*
+基本介绍：闭包就是一个函数和与其相关的引用环境组合的一个整体(实体)；
+1.AddUpper是一个函数，返回的数据类型是func (int) int;
+2.说明：AddUpper返回一个匿名函数，匿名函数引用到函数外的n，该匿名函数和n形成的整体就是一个闭包；
+3.可以这样理解闭包，闭包是类，函数是操作，n是字段；函数和n构成闭包；
+4.
+*/
+//累加器
+func AddUpper() func(int) int {
+	n := 10
+	return func(x int) int {
+		n += x
+		return n
+	}
+}
+
+//使用闭包
+func ClosureFunc() {
+	f := AddUpper()
+	fmt.Println(f(1)) //11
+	fmt.Println(f(2)) //13
+	fmt.Println(f(3)) //16
+}
+
+//7.defer延时机制
+/*
+在函数中，程序员经常需要创建资源，如数据库连接，文件句柄，锁等，为了在执行完毕后，及时释放资源，Go提供了defer；
+*/
+func deferFunc(n1, n2 int) {
+	//1.当执行到defer时，暂时不执行，会将defer后面的语句压入独立的栈（defer栈）
+	//2.当函数执行完毕后，再从defer栈，按照先入后出的方式出栈；
+	defer fmt.Println("3. n1=", n1)
+	defer fmt.Println("2. n1=", n2)
+	res := n1 + n2
+	fmt.Println("1. res=", res)
+}
 func main() {
 	//packageExample()
-	returnExample()
+	//returnExample()
+	//anonymousFunc()
+	//ClosureFunc()
+	deferFunc(10, 20)
 }
